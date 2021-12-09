ca3ba8c45e8405d922c79b35561a8b8768ca922b
Hidex <seryga38120@mail.ru>
2021-12-09 15:05:11 +0700

    Начало

---
+++ GIT_TEST/GIT_TEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GIT_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
