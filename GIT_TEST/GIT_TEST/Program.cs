e3c0300e84ec729e6adb73f5d5d27e6f0008e2ae
Hidex <seryga38120@mail.ru>
2021-12-10 11:48:49 +0700

    Сохранение исходного мастера

--- GIT_TEST/GIT_TEST/Program.cs
+++ GIT_TEST/GIT_TEST/Program.cs
@@ -7,13 +7,11 @@ namespace GIT_TEST
     {
         private static double A, B;
         static void Main(string[] args)
-        
         {
 
 
             while (true)
             {
-
                 Console.WriteLine("          MENU            ");
                 Console.WriteLine("1. Ввести A");
                 Console.WriteLine("2. Ввести B");
@@ -23,7 +21,6 @@ namespace GIT_TEST
                 Console.WriteLine("6. Выполнить операцию «/»");
                 Console.WriteLine("Выберите пункт меню");
                 char ch = char.Parse(Console.ReadLine());
-
                 switch (ch)
                 {
                     case '1':
@@ -35,7 +32,15 @@ namespace GIT_TEST
                     case '3':
                         Plus(A, B);
                         break;
-                   
+                    case '4':
+                        Minus(A, B);
+                        break;
+                    case '5':
+                        Mult(A, B);
+                        break;
+                    case '6':
+                        Del(A, B);
+                        break;
                     default:
                         Console.WriteLine("Произошла ошибка, введите заного");
                         break;
@@ -50,7 +55,6 @@ namespace GIT_TEST
 
 
             }
-
             static void InputB(string input)
             {
 
@@ -59,13 +63,22 @@ namespace GIT_TEST
 
 
             }
-
             static void Plus(double a, double b)
             {
                 Console.WriteLine(a + b);
             }
-
-           
+            static void Minus(double a, double b)
+            {
+                Console.WriteLine(a - b);
+            }
+            static void Mult(double a, double b)
+            {
+                Console.WriteLine(a * b);
+            }
+            static void Del(double a, double b)
+            {
+                Console.WriteLine(a / b);
+            }
 
         }
     }
